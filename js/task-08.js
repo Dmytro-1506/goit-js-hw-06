bcd23d03b6aa349987d40a07c38dec62ff064e16
Dmytro-1506 <kyunlik@gmail.com>
2022-11-13 19:57:41 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+    const objectForm = {
+    };
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert("Please fill in all the fields!");
+    }
+    objectForm.email = email.value;
+    objectForm.password = password.value;
+    console.log(objectForm);
+    event.currentTarget.reset();
+}
+
+
 
