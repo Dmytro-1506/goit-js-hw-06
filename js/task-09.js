725479e711c3bb5a578ed52dac281fabcdd8d55f
Dmytro-1506 <kyunlik@gmail.com>
2022-11-13 20:14:03 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const changeColorBtn = document.querySelector('button');
+const nameOfColor = document.querySelector('span');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+  const newColor = getRandomHexColor();
+
+  body.setAttribute('style', `background-color: ${newColor}`);
+  nameOfColor.textContent = newColor;
+  console.log(newColor);
+  console.log('sdfg');
 }
