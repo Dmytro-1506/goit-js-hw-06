e21b9ffe18c3fa6d12c0df70efec35f5cd271a29
Dmytro-1506 <kyunlik@gmail.com>
2022-11-14 20:23:35 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,58 +1,10 @@
-// const categoriesArr = document.querySelectorAll(".item");
+const categoties = document.querySelectorAll('.item');
 
-const categotiesList = document.querySelector('#categories');
-const categoriesArr = categotiesList.children;
-
-console.log(categoriesArr);
-// Number of categories: 3
-
-console.log(`Number of categories: ${categoriesArr.length}`);
+console.log(`Number of categories: ${categories.length}`);
 console.log("");
 
-const animals = categotiesList.firstElementChild;
-const products = animals.nextElementSibling;
-const technologies = products.nextElementSibling;
-
-
-function nameOfCategorie(element) {
-    return element.textContent;
-};
-
-function numberOfItems(element) { 
-    return element.children.length;
-};
-
-console.log(`Category: ${nameOfCategorie(animals.firstElementChild)}`);
-console.log(`Elements: ${numberOfItems(animals.lastElementChild)}`);
-console.log("");
-console.log(`Category: ${nameOfCategorie(products.firstElementChild)}`);
-console.log(`Elements: ${numberOfItems(products.lastElementChild)}`);
-console.log("");
-console.log(`Category: ${nameOfCategorie(technologies.firstElementChild)}`);
-console.log(`Elements: ${numberOfItems(technologies.lastElementChild)}`);
-console.log("");
-
-// function categoriesList(arr) {
-//     let titleOfCategorie = "";
-//     let numberOfitems = 0;
-
-//     arr.forEach(element => {
-//         titleOfCategorie = element.querySelector("h2").textContent;
-//         numberOfitems = element.querySelector("ul").querySelectorAll('li').length;
-
-        // console.log(`Category: ${titleOfCategorie}`);
-        // console.log(`Elements: ${numberOfitems}`);
-        // console.log("");
-//     });
-// }
-
-// categoriesList(categoriesArr);
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
+categoties.forEach((element) => {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.lastElementChild.children.length}`);
+    console.log("");
+});
