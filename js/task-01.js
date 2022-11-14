b5a666943203922f70599c728317eade648835c1
Dmytro-1506 <kyunlik@gmail.com>
2022-11-14 18:42:43 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,52 @@
-const categoriesArr = document.querySelectorAll(".item");
+// const categoriesArr = document.querySelectorAll(".item");
 
+const categotiesList = document.querySelector('#categories');
+const categoriesArr = categotiesList.children;
+
+console.log(categoriesArr);
 // Number of categories: 3
 
 console.log(`Number of categories: ${categoriesArr.length}`);
 console.log("");
 
-function categoriesList(arr) {
-    let titleOfCategorie = "";
-    let numberOfitems = 0;
+const animals = categotiesList.firstElementChild;
+const products = animals.nextElementSibling;
+const technologies = products.nextElementSibling;
 
-    arr.forEach(element => {
-        titleOfCategorie = element.querySelector("h2").textContent;
-        numberOfitems = element.querySelector("ul").querySelectorAll('li').length;
 
-        console.log(`Category: ${titleOfCategorie}`);
-        console.log(`Elements: ${numberOfitems}`);
-        console.log("");
-    });
-}
+function nameOfCategorie(element) {
+    return element.textContent;
+};
 
-categoriesList(categoriesArr);
+function numberOfItems(element) { 
+    return element.children.length;
+};
+
+console.log(`Category: ${nameOfCategorie(animals.firstElementChild)}`);
+console.log(`Elements: ${numberOfItems(animals.lastElementChild)}`);
+console.log("");
+console.log(`Category: ${nameOfCategorie(products.firstElementChild)}`);
+console.log(`Elements: ${numberOfItems(products.lastElementChild)}`);
+console.log("");
+console.log(`Category: ${nameOfCategorie(technologies.firstElementChild)}`);
+console.log(`Elements: ${numberOfItems(technologies.lastElementChild)}`);
+console.log("");
+
+// function categoriesList(arr) {
+//     let titleOfCategorie = "";
+//     let numberOfitems = 0;
+
+//     arr.forEach(element => {
+//         titleOfCategorie = element.querySelector("h2").textContent;
+//         numberOfitems = element.querySelector("ul").querySelectorAll('li').length;
+
+        // console.log(`Category: ${titleOfCategorie}`);
+        // console.log(`Elements: ${numberOfitems}`);
+        // console.log("");
+//     });
+// }
+
+// categoriesList(categoriesArr);
 
 // Category: Animals
 // Elements: 4
